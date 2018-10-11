d7a5921f79a6ed69002a938565a05d7b5d023f14
gwatson86 <gwatson86@gmail.com>
2018-10-11 14:22:54 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,50 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion() {
+  document.querySelector('.question-container').innerHTML = questions[0].questionText;
+}
+
+function askQuestionThen(time) {
+  question = questions[0];
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve();
+    }, time)
+  })
+}
+
+function removeQuestion() {
+  document.querySelector('.question-container').innerHTML = "";
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  appendQuestion();
+  return askQuestionThen(time).then(removeQuestion);
+}
+
+function trueAndFalseButtons() {
+  return document.querySelector('.true-false-list').querySelectorAll('.btn');
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function(button) {
+    if (button.classList.contains('hide')) {
+      button.classList.remove('hide');
+    } else {
+      button.classList.add('hide');
+    }
+  })
+}
+
+function displayQuestionOnClick() {
+  const button = document.querySelector('.waves-light')
+
+  button.addEventListener('click', function() {
+    toggleTrueAndFalseButtons();
+    askQuestionThenRemoveQuestion(5000);
+  })
+}
+
+displayQuestionOnClick();
