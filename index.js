2584683325a52e2bd77a34e3c5772934c7129010
EthanFe <sneakcow@gmail.com>
2018-10-10 22:35:08 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,50 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion() {
+  document.getElementsByClassName("question-container")[0].innerHTML = questions[0].questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve()
+    }, time )
+  })
+}
+
+function removeQuestion() {
+  document.getElementsByClassName("question-container")[0].innerHTML = ""
+  toggleTrueAndFalseButtons()
+  return new Promise((resolve, reject) => {})
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  appendQuestion()
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  return Array.from(document.getElementsByClassName("true-false-list")[0].children)
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function(element) {
+    if (element.classList.contains("hide"))
+      element.classList.remove("hide")
+    else
+      element.classList.add("hide")
+  })
+}
+
+function displayQuestionOnClick() {
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+displayQuestionOnClick();
